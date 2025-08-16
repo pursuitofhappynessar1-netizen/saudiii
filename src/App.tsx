054cb980169e3d03bed10c54d61bd4aecfac3b31
pursuitofhappynessar1-netizen <pursuit.of.happyness.ar1@gmail.com>
2025-08-16 03:51:38 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import AboutAssociation from './pages/AboutAssociation';
 import WhoShouldAttend from './pages/WhoShouldAttend';
 import Flights from './pages/Flights';
 import VisaRequirements from './pages/VisaRequirements';
-import AboutConference from './pages/AboutConference';
+import AboutConference from './pages/AboutScientificCommittee';
 
 // English components
 import LayoutEn from './components/en/LayoutEn';
